defeab0d5c82357e431cb2018a979449d8d8b4a5
Suraj Parmar <golurajput87080@gmail.com>
2025-12-25 21:02:25 +0530

    Khuch To Kiya h

--- GSAP_REACT_PROJECT/src/Components/Navigation/FullScreenNav.jsx
+++ GSAP_REACT_PROJECT/src/Components/Navigation/FullScreenNav.jsx
@@ -2,18 +2,77 @@ import React from "react";
 import assets from "../../assets/asset";
 const FullScreenNav = () => {
   return (
-    <div id="fullScreenNav" className="text-white h-screen py-40 w-full absolute bg-black">
+    <div id="fullScreenNav" className="text-white h-screen py-20 w-full absolute bg-black">
       <div className="" >
-        <div className="link border-y">
+        <div className="relative border-y link">
             <h1 className=" font-semibold text-white text-8xl pb-3.5 text-center  uppercase">PROJECTS</h1>
-        <div className="absolute overflow-auto  flex"> 
-            <div className="flex items-center">
-                <h2 className=" whitespace-nowrap font-semibold text-white text-8xl pb-3.5 text-center  uppercase">TO SEE EVERYTHING</h2>
-                <img className="h-20 shrink-0 w-100  object-cover rounded-full" src={assets.NavImg2} alt="" />
-                <h2 className=" whitespace-nowrap font-semibold text-white text-8xl pb-3.5 text-center  uppercase">TO SEE EVERYTHING</h2>
-                <img className="h-20 shrink-0 w-100 object-cover rounded-full" src={assets.NavImg3} alt="" />
+        <div className="absolute  text-black bg-[#D3FD50] flex top-0 moveLink"> 
+            <div className="moveX flex overflow-hidden items-center ">
+                <h2 className=" text-black  whitespace-nowrap font-semibold  text-8xl pb-3.5 text-center  uppercase">TO SEE EVERYTHING</h2>
+                <img className="h-20  w-100  object-cover rounded-full" src={assets.NavImg2} alt="" />
+                <h2 className=" text-black whitespace-nowrap font-semibold text-8xl pb-3.5 px-0.5 text-center  uppercase">TO SEE EVERYTHING</h2>
+                <img className=" h-20  w-100 object-cover rounded-full" src={assets.NavImg3} alt="" />
             </div>
-            
+            <div className="moveX flex overflow-hidden items-center ">
+                <h2 className=" text-black  whitespace-nowrap font-semibold  text-8xl pb-3.5 text-center  uppercase">TO SEE EVERYTHING</h2>
+                <img className="h-20  w-100  object-cover rounded-full" src={assets.NavImg2} alt="" />
+                <h2 className=" text-black whitespace-nowrap font-semibold text-8xl pb-3.5 text-center  uppercase">TO SEE EVERYTHING</h2>
+                <img className=" h-20  w-100 object-cover rounded-full" src={assets.NavImg3} alt="" />
+            </div>
+        </div>
+
+        </div>
+        <div className="relative border-y link">
+            <h1 className=" font-semibold text-white text-8xl pb-3.5 text-center  uppercase">PROJECTS</h1>
+        <div className="absolute  text-black bg-[#D3FD50] flex top-0 moveLink"> 
+            <div className="moveX flex overflow-hidden items-center ">
+                <h2 className=" text-black  whitespace-nowrap font-semibold  text-8xl pb-3.5 text-center  uppercase">TO SEE EVERYTHING</h2>
+                <img className="h-20  w-100  object-cover rounded-full" src={assets.NavImg2} alt="" />
+                <h2 className=" text-black whitespace-nowrap font-semibold text-8xl pb-3.5 text-center  uppercase">TO SEE EVERYTHING</h2>
+                <img className=" h-20  w-100 object-cover rounded-full" src={assets.NavImg3} alt="" />
+            </div>
+            <div className="moveX flex overflow-hidden items-center ">
+                <h2 className=" text-black  whitespace-nowrap font-semibold  text-8xl pb-3.5 text-center  uppercase">TO SEE EVERYTHING</h2>
+                <img className="h-20  w-100  object-cover rounded-full" src={assets.NavImg2} alt="" />
+                <h2 className=" text-black whitespace-nowrap font-semibold text-8xl pb-3.5 text-center  uppercase">TO SEE EVERYTHING</h2>
+                <img className=" h-20  w-100 object-cover rounded-full" src={assets.NavImg3} alt="" />
+            </div>
+        </div>
+
+        </div>
+         <div className="relative border-y link">
+            <h1 className=" font-semibold text-white text-8xl pb-3.5 text-center  uppercase">PROJECTS</h1>
+        <div className="absolute  text-black bg-[#D3FD50] flex top-0 moveLink"> 
+            <div className="moveX flex overflow-hidden items-center ">
+                <h2 className=" text-black  whitespace-nowrap font-semibold  text-8xl pb-3.5 text-center  uppercase">TO SEE EVERYTHING</h2>
+                <img className="h-20  w-100  object-cover rounded-full" src={assets.NavImg2} alt="" />
+                <h2 className=" text-black whitespace-nowrap font-semibold text-8xl pb-3.5 text-center mx-0.5 uppercase">TO SEE EVERYTHING</h2>
+                <img className=" h-20  w-100 object-cover rounded-full" src={assets.NavImg3} alt="" />
+            </div>
+            <div className="moveX flex overflow-hidden items-center ">
+                <h2 className=" text-black  whitespace-nowrap font-semibold  text-8xl pb-3.5 text-center  uppercase">TO SEE EVERYTHING</h2>
+                <img className="h-20  w-100  object-cover rounded-full" src={assets.NavImg2} alt="" />
+                <h2 className=" text-black whitespace-nowrap font-semibold text-8xl pb-3.5 text-center  uppercase">TO SEE EVERYTHING</h2>
+                <img className=" h-20  w-100 object-cover rounded-full" src={assets.NavImg3} alt="" />
+            </div>
+        </div>
+
+        </div>
+         <div className="relative border-y link">
+            <h1 className=" font-semibold text-white text-8xl pb-3.5 text-center  uppercase">PROJECTS</h1>
+        <div className="absolute  text-black bg-[#D3FD50] flex top-0 moveLink"> 
+            <div className="moveX flex overflow-hidden items-center ">
+                <h2 className=" text-black  whitespace-nowrap font-semibold  text-8xl pb-3.5 text-center  uppercase">TO SEE EVERYTHING</h2>
+                <img className="h-20  w-100  object-cover rounded-full" src={assets.NavImg2} alt="" />
+                <h2 className=" text-black whitespace-nowrap font-semibold text-8xl pb-3.5 text-center  uppercase">TO SEE EVERYTHING</h2>
+                <img className=" h-20  w-100 object-cover rounded-full" src={assets.NavImg3} alt="" />
+            </div>
+            <div className="moveX flex overflow-hidden items-center ">
+                <h2 className=" text-black  whitespace-nowrap font-semibold  text-8xl pb-3.5 text-center  uppercase">TO SEE EVERYTHING</h2>
+                <img className="h-20  w-100  object-cover rounded-full" src={assets.NavImg2} alt="" />
+                <h2 className=" text-black whitespace-nowrap font-semibold text-8xl pb-3.5 text-center  uppercase">TO SEE EVERYTHING</h2>
+                <img className=" h-20  w-100 object-cover rounded-full" src={assets.NavImg3} alt="" />
+            </div>
         </div>
 
         </div>
